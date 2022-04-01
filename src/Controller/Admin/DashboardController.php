ffffa7c81edfdfaf2c2bfdcccf4deff7ae40025b
Naima <touraghen@gmail.com>
2022-04-01 14:24:40 +0200

    fix: dashboard admin + add crud

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Admin;
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator)
+    {
+
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url=$this->adminUrlGenerator->setController(CommentCrudController::class)->generateUrl();
+        
+        return $this->redirect($url);
+        
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('HeplApp');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Users');
+        
+        yield MenuItem::subMenu('Users', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Add users', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show users', 'fas fa-eye', User::class)
+        ]);
+        
+
+        yield MenuItem::section('Posts');
+        yield MenuItem::subMenu('Posts', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Add posts', 'fas fa-plus', Post::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show posts', 'fas fa-eye', Post::class)
+        ]);
+        yield MenuItem::section('Comments');
+        yield MenuItem::subMenu('Comments', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Add comment', 'fas fa-plus', Comment::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show comment', 'fas fa-eye', Comment::class)
+        ]);
+
+        
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
